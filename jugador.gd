549b813f3957ced656d0569066d973580517dd6e
MetaUwU <leonelymichi@gmail.com>
2026-01-31 06:46:42 -0300

    Agrege La mecanica de las mascaras
    Falta agregar balas que interactuen con las mascaras

--- jugador.gd
+++ jugador.gd
@@ -8,3 +8,26 @@ func _physics_process(delta: float) -> void:
 	direccion = Vector2(Input.get_axis("Mover_Izquierda","Mover_Derecha"),Input.get_axis("Mover_Arriba","Mover_Abajo" )).normalized()
 	velocity = direccion * SPEED
 	move_and_slide()
+	Cambiar_Mascara()
+
+#recontra controlar, esta seguro RE mal
+func Cambiar_Mascara():
+	if Input.is_action_just_pressed("Mascara_1"):
+		set_collision_layer_value(1, true)
+		set_collision_layer_value(2, false)
+		set_collision_layer_value(3, false)
+
+
+		print("Mascara 1")
+	
+	if Input.is_action_just_pressed("Mascara_2"):
+		set_collision_layer_value(2, true)
+		set_collision_layer_value(1, false)
+		set_collision_layer_value(3, false)
+		print("mascara 2")
+		
+	if Input.is_action_just_pressed("Mascara_3"):
+		set_collision_layer_value(3, true)
+		set_collision_layer_value(2, false)
+		set_collision_layer_value(1, false)
+		print("Mascara 31sds")
