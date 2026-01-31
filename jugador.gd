00dfda2fdd29ce5ba50809e3cef12002f13b99bb
MetaUwU <leonelymichi@gmail.com>
2026-01-31 06:21:58 -0300

    Se añadio bala y Colision
    Falta arreglar colisiones

--- jugador.gd
+++ jugador.gd
@@ -5,9 +5,6 @@ var direccion
 
 func _physics_process(delta: float) -> void:
 	
-	#var V_direccion =
-	#var H_direccion = 
-	
 	direccion = Vector2(Input.get_axis("Mover_Izquierda","Mover_Derecha"),Input.get_axis("Mover_Arriba","Mover_Abajo" )).normalized()
 	velocity = direccion * SPEED
 	move_and_slide()
