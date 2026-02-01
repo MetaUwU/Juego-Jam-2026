e6e5c454e9e13472e38e3df1181ea379d2f614a1
MetaUwU <leonelymichi@gmail.com>
2026-01-31 23:48:58 -0300

    ahora noli tiene 3 hijos
    dispara los 3 tipos de bala

--- enemigo.gd
+++ enemigo.gd
@@ -6,15 +6,19 @@ const bala_3 = preload("res://Escenas/Balas/bala_3.tscn")
 
 @onready var shoot_timer = $shoot_timer
 @onready var shoot_timer2 = $shoot_timer2
+@onready var shoot_timer3 = $shoot_timer3
 @onready var rotater = $Rotater
 @onready var rotater2 = $Rotater2
 @onready var rotater3 = $Rotater3
 
 const rotate_speed = 100
+const rotate_speed2 = 150.5
+const rotate_speed3 = 200
 const shooter_timer_wait_time = 0.2
 const spawn_point_count = 4
 const radius = 100
 const shooter_timer2_wait_time = 0.2
+const shooter_timer3_wait_time = 0.05
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var step = 2 * PI / spawn_point_count
@@ -28,10 +32,17 @@ func _ready() -> void:
 
 	for p in range(spawn_point_count):
 		var spawn_point = Node2D.new()
-		var pos = Vector2(radius, 0).rotated(step * 2)
+		var pos = Vector2(radius, 0).rotated(step * 1)
 		spawn_point.position = pos
 		spawn_point.rotation = pos.angle()
 		rotater.add_child(spawn_point)
+
+	for t in range(spawn_point_count):
+		var spawn_point = Node2D.new()
+		var pos = Vector2(250, 0).rotated(step * 1)
+		spawn_point.position = pos
+		spawn_point.rotation = pos.angle()
+		rotater3.add_child(spawn_point)
 
 	shoot_timer.wait_time = shooter_timer_wait_time
 	shoot_timer.start()
@@ -39,13 +50,20 @@ func _ready() -> void:
 	shoot_timer2.wait_time = shooter_timer2_wait_time
 	shoot_timer2.start()
 	print("timer2")
+	
+	shoot_timer3.wait_time = shooter_timer3_wait_time
+	shoot_timer3.start()
+	
 func _process(delta: float) -> void:
 	var new_rotation = rotater.rotation_degrees + rotate_speed * delta
 	rotater.rotation_degrees = fmod(new_rotation, 360)
 	
-	var new_rotation2 = rotater2.rotation_degrees - rotate_speed * delta
+	var new_rotation2 = rotater2.rotation_degrees - rotate_speed2 * delta
 	rotater2.rotation_degrees = fmod(new_rotation2, 360)
 
+	var new_rotation3 = rotater3.rotation_degrees - rotate_speed3 * delta
+	rotater3.rotation_degrees = fmod(new_rotation3, 360)
+	
 func _on_shoot_timer_timeout() -> void:
 	for s in rotater.get_children():
 		var b1 = bala_1.instantiate()
@@ -61,3 +79,12 @@ func _on_shoot_timer_2_timeout() -> void:
 		b2.position = d.global_position
 		b2.rotation = d.global_rotation
 		print("noli2")
+
+
+func _on_shoot_timer_3_timeout() -> void:
+	for l in rotater3.get_children():
+		var b3 = bala_3.instantiate()
+		get_tree().root.add_child(b3)
+		b3.position = l.global_position
+		b3.rotation = l.global_rotation
+		print("noli3")
