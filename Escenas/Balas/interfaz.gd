d0a0d957db0941c86aa2ef66e4781a5e602f8848
MetaUwU <leonelymichi@gmail.com>
2026-02-01 10:42:44 -0300

    Agregadas vidas de jugador

--- Escenas/Balas/interfaz.gd
+++ Escenas/Balas/interfaz.gd
@@ -3,10 +3,22 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	$TextureRect.show()
+	$TextureRect2.show()
+	$TextureRect3.show()
 	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	$Label.text = str(3)
+	if VidasJugador.hp == 2:
+		$TextureRect3.hide()
+		$Label.text = str(2)
+	if VidasJugador.hp == 1:
+		$Label.text = str(1)
+		$TextureRect2.hide()
+	if VidasJugador.hp == 0:
+		$TextureRect.hide()
+	
+	
