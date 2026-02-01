22cf7bccb93943f6b593749cc26ed13ab67219c4
MetaUwU <leonelymichi@gmail.com>
2026-01-31 23:07:19 -0300

    Ahora dispara 1 tipo de bala

--- Escenas/Balas/bala.gd
+++ Escenas/Balas/bala.gd
@@ -1,12 +1,16 @@
-extends RigidBody2D
+extends Node2D
 
-
+const speed = 100
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-
+func _process(delta):
+	position += transform.x * speed * delta
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print("daño")
+
+func _on_killtimer_timeout():
+	queue_free()
--- enemigo.gd
+++ enemigo.gd
@@ -1,24 +1,39 @@
-extends StaticBody2D
+extends Node2D
 
-@onready var bala_1 = load("res://Escenas/Balas/bala.tscn")
-@onready var bala_2 = load("res://Escenas/Balas/bala_2.tscn")
-@onready var bala_3 = preload("res://Escenas/Balas/bala_3.tscn")
+const bala_1 = preload("res://Escenas/Balas/bala.tscn")
+const bala_2 = preload("res://Escenas/Balas/bala_2.tscn")
+const bala_3 = preload("res://Escenas/Balas/bala_3.tscn")
+
+@onready var shoot_timer = $shoot_timer
+@onready var rotater = $Rotater
+
+
+const rotate_speed = 100
+const shooter_timer_wait_time = 0.2
+const spawn_point_count = 4
+const radius = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	rotate(.5 * delta)
-	Spawnear_balas()
+	var step = 2 * PI / spawn_point_count
 	
-	#tratando de Spawnear balas aun
-func Spawnear_balas():
-	var b3= bala_3.instantiate()
-	b3.position = self.position
-	b3.velocity = 20
-	
-	
-	get_parent().add_child(b3)
+	for i in range(spawn_point_count):
+		var spawn_point = Node2D.new()
+		var pos = Vector2(radius, 0).rotated(step * 1)
+		spawn_point.position = pos
+		spawn_point.rotation = pos.angle()
+		rotater.add_child(spawn_point)
 
-	
+	shoot_timer.wait_time = shooter_timer_wait_time
+	shoot_timer.start()
+
+func _process(delta: float) -> void:
+	var new_rotation = rotater.rotation_degrees + rotate_speed * delta
+	rotater.rotation_degrees = fmod(new_rotation, 360)
+
+func _on_shoot_timer_timeout() -> void:
+	for s in rotater.get_children():
+		var b1 = bala_1.instantiate()
+		get_tree().root.add_child(b1)
+		b1.position = s.global_position
+		b1.rotation = s.global_rotation
