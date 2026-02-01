05e34f2fc3646b62c6bff38cd1c07af2f4247baf
MetaUwU <leonelymichi@gmail.com>
2026-02-01 10:56:23 -0300

    Agrege timer de ganar, y el menu de ganar

---
+++ Escenas/camera_2d.gd
@@ -0,0 +1,14 @@
+extends Camera2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Ganar.start()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_ganar_timeout() -> void:
+	get_tree().change_scene_to_file("res://menu_de_ganar.tscn")
